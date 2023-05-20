4d696d901b962ff164df0f2f32d10bd850add167
TheRetikGM <theretikgm@gmail.com>
2023-05-20 14:46:34 +0200

    Initial commit

---
+++ init.lua
@@ -0,0 +1,209 @@
+
+-- Install plugins
+require('plugins')
+
+-- [[ Setting options ]]
+-- See `:help vim.o`
+
+-- Set highlight on search
+vim.o.hlsearch = false
+-- Make line numbers default
+vim.wo.number = true
+-- Enable mouse mode
+vim.o.mouse = 'a'
+-- Enable break indent
+vim.o.breakindent = true
+-- Save undo history
+vim.o.undofile = true
+-- Case insensitive searching UNLESS /C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+-- Decrease update time
+vim.o.updatetime = 250
+vim.wo.signcolumn = 'yes'
+-- Set colorscheme
+vim.o.termguicolors = true
+vim.cmd [[colorscheme tokyonight]]
+-- Set completeopt to have a better completion experience
+vim.o.completeopt = 'menuone,noselect,preview'
+-- Set relative line numbering
+vim.o.relativenumber = true
+-- Set command line height to 0
+vim.o.cmdheight = 0
+-- Highlight current line
+vim.o.cursorline = true
+
+-- [[ Basic Keymaps ]]
+-- Set <space> as the leader key
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- Setup plugins
+require('plugin_setups')
+
+-- Keymaps for better default experience
+-- See `:help vim.keymap.set()`
+vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+-- Remap for dealing with word wrap
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank()
+  end,
+  group = highlight_group,
+  pattern = '*',
+})
+
+-- LSP settings.
+--  This function gets run when an LSP connects to a particular buffer.
+local on_attach = function(_, bufnr)
+  -- NOTE: Remember that lua is a real programming language, and as such it is possible
+  -- to define small helper and utility functions so you don't have to repeat yourself
+  -- many times.
+  --
+  -- In this case, we create a function that lets us more easily define mappings specific
+  -- for LSP related items. It sets the mode, buffer and description for us each time.
+  local nmap = function(keys, func, desc)
+    if desc then
+      desc = 'LSP: ' .. desc
+    end
+
+    vim.keymap.set('n', keys, func, { buffer = bufnr, desc = desc })
+  end
+
+  nmap('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+  nmap('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+
+  nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
+  nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+  nmap('gI', vim.lsp.buf.implementation, '[G]oto [I]mplementation')
+  nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
+  nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+  nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+
+  -- See `:help K` for why this keymap
+  nmap('K', vim.lsp.buf.hover, 'Hover Documentation')
+  -- nmap('<C-k>', vim.lsp.buf.signature_help, 'Signature Documentation')
+
+  -- Lesser used LSP functionality
+  nmap('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+  nmap('<leader>wa', vim.lsp.buf.add_workspace_folder, '[W]orkspace [A]dd Folder')
+  nmap('<leader>wr', vim.lsp.buf.remove_workspace_folder, '[W]orkspace [R]emove Folder')
+  nmap('<leader>wl', function()
+    print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  end, '[W]orkspace [L]ist Folders')
+
+  -- Create a command `:Format` local to the LSP buffer
+  vim.api.nvim_buf_create_user_command(bufnr, 'Format', function(_)
+    vim.lsp.buf.format()
+  end, { desc = 'Format current buffer with LSP' })
+end
+
+-- Enable the following language servers
+--  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
+--
+--  Add any additional override configuration in the following tables. They will be passed to
+--  the `settings` field of the server config. You must look up that documentation yourself.
+local servers = {
+  clangd = {},
+  lua_ls = {
+    Lua = {
+      workspace = { checkThirdParty = false },
+      telemetry = { enable = false },
+    },
+  },
+}
+
+-- Setup neovim lua configuration
+require('neodev').setup()
+--
+-- nvim-cmp supports additional completion capabilities, so broadcast that to servers
+local capabilities = vim.lsp.protocol.make_client_capabilities()
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
+
+-- Setup mason so it can manage external tooling
+require('mason').setup()
+
+-- Ensure the servers above are installed
+local mason_lspconfig = require 'mason-lspconfig'
+
+mason_lspconfig.setup {
+  ensure_installed = vim.tbl_keys(servers),
+}
+
+mason_lspconfig.setup_handlers {
+  function(server_name)
+    -- if server_name == "rust_analyzer" then
+    --   return
+    -- end
+    require('lspconfig')[server_name].setup {
+      capabilities = capabilities,
+      on_attach = on_attach,
+      settings = servers[server_name],
+    }
+  end,
+}
+
+-- Turn on lsp status information
+require('fidget').setup()
+
+-- nvim-cmp setup
+local cmp = require 'cmp'
+local luasnip = require 'luasnip'
+
+cmp.setup {
+  snippet = {
+    expand = function(args)
+      luasnip.lsp_expand(args.body)
+    end,
+  },
+  mapping = cmp.mapping.preset.insert {
+    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<CR>'] = cmp.mapping.confirm(),
+    ['<Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+    ['<S-Tab>'] = cmp.mapping(function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      elseif luasnip.jumpable(-1) then
+        luasnip.jump(-1)
+      else
+        fallback()
+      end
+    end, { 'i', 's' }),
+  },
+  sources = {
+    { name = 'nvim_lsp' },
+    { name = 'luasnip' },
+    -- File paths
+    { name = 'path' },
+    -- Function parameters while typing
+    { name = 'nvim_lsp_signature_help' },
+    -- Source current buffer.
+    { name = 'buffer' },
+    { name = 'vsnip' },
+  },
+}
+
+-- Set my custom eybindins for plugins
+require('keybindings')
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
---
+++ lua/keybindings.lua
@@ -0,0 +1,160 @@
+-- See `:help telescope.builtin`
+vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
+vim.keymap.set('n', '<leader><space>', require('telescope.builtin').commands, { desc = '[ ] Find commands' })
+vim.keymap.set('n', '<leader>/', function()
+  -- You can pass additional configuration to telescope to change theme, layout, etc.
+  require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+    winblend = 10,
+    previewer = false,
+  })
+end, { desc = '[/] Fuzzily search in current buffer]' })
+
+vim.keymap.set('n', '<leader>sf', require('telescope.builtin').find_files, { desc = '[S]earch [F]iles' })
+vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc = '[S]earch [H]elp' })
+vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
+vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
+vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+vim.keymap.set('n', '<leader>sk', require('telescope.builtin').keymaps, { desc = '[S]earch [K]eymaps' })
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float)
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist)
+
+-- BufferLine keymaps
+vim.keymap.set('n', 'H', ':BufferLineCyclePrev<cr>', { desc = 'Move to previous buffer' })
+vim.keymap.set('n', 'L', ':BufferLineCycleNext<cr>', { desc = 'Move to next buffer' })
+vim.keymap.set('n', '<A-1>', ':BufferLineGoToBuffer 1<cr>', { desc = 'Go to BufferLine tab 1' })
+vim.keymap.set('n', '<A-2>', ':BufferLineGoToBuffer 2<cr>', { desc = 'Go to BufferLine tab 2' })
+vim.keymap.set('n', '<A-3>', ':BufferLineGoToBuffer 3<cr>', { desc = 'Go to BufferLine tab 3' })
+vim.keymap.set('n', '<A-4>', ':BufferLineGoToBuffer 4<cr>', { desc = 'Go to BufferLine tab 4' })
+vim.keymap.set('n', '<A-5>', ':BufferLineGoToBuffer 5<cr>', { desc = 'Go to BufferLine tab 5' })
+vim.keymap.set('n', '<A-6>', ':BufferLineGoToBuffer 6<cr>', { desc = 'Go to BufferLine tab 6' })
+vim.keymap.set('n', '<A-7>', ':BufferLineGoToBuffer 7<cr>', { desc = 'Go to BufferLine tab 7' })
+vim.keymap.set('n', '<A-8>', ':BufferLineGoToBuffer 8<cr>', { desc = 'Go to BufferLine tab 8' })
+vim.keymap.set('n', '<A-9>', ':BufferLineGoToBuffer 9<cr>', { desc = 'Go to BufferLine tab 9' })
+vim.keymap.set('n', '<A-S-h>', ':BufferLineMovePrev<cr>', { desc = 'Move buffer to the left' })
+vim.keymap.set('n', '<A-S-l>', ':BufferLineMoveNext<cr>', { desc = 'Move buffer to the right' })
+
+-- Buffer actions
+vim.keymap.set('n', '<leader>c', ':Bwipeout<cr>', { desc = 'Close current buffer' })
+vim.keymap.set('n', '<leader>cc', ':Bwipeout!<cr>', { desc = 'Forcefuly Close current buffer' })
+vim.keymap.set('n', '<leader>w', ':w<cr>', { desc = 'Write changes to buffer' })
+vim.keymap.set('n', '<leader>q', ':q<cr>', { desc = 'Quit' })
+vim.keymap.set('n', '<leader>qq', ':qa<cr>', { desc = 'Quit all' })
+vim.keymap.set('n', '<A-d>', ':t .<cr>', { desc = 'Duplicate line below' })
+vim.keymap.set('v', '<A-d>', ':\'<,\'>t \'><cr>', { desc = 'Duplicate selection below' })
+vim.keymap.set('n', '<Tab>', [[v>]], { desc = 'Indent line' })
+vim.keymap.set('n', '<S-Tab>', [[v<]], { desc = 'Unindent line' })
+vim.keymap.set('v', '<Tab>', [[>gv]], { desc = 'Indent block and select it again' })
+vim.keymap.set('v', '<S-Tab>', [[<gv]], { desc = 'Indent block and select it again' })
+vim.keymap.set('i', '<S-Tab>', [[<BS>]], { desc = 'Provide un-tab in insert mode' })
+
+-- Buffer shortcuts
+vim.keymap.set('n', '<leader>st', ':set filetype=', { desc = '[S]et buffer File[T]ype'})
+vim.keymap.set('n', '<leader>sb', ':set background=', { desc = '[S]et [B]ackground' })
+vim.keymap.set('n', '<leader>sc', ':colorscheme ', { desc = '[S]et [C]olorscheme' })
+
+-- Move between splits
+vim.keymap.set('n', '<C-h>', ':wincmd h<cr>', { desc = 'Move to left split' })
+vim.keymap.set('n', '<C-j>', ':wincmd j<cr>', { desc = 'Move to down split' })
+vim.keymap.set('n', '<C-k>', ':wincmd k<cr>', { desc = 'Move to up split' })
+vim.keymap.set('n', '<C-l>', ':wincmd l<cr>', { desc = 'Move to right split' })
+vim.keymap.set('n', '<C-q>', ':wincmd q<cr>', { desc = 'Close window' })
+
+-- Toggle NeoTree
+local neotree_open = false
+local function neotree_toggle(_)
+  neotree_open = not neotree_open
+  vim.cmd('Neotree toggle')
+end
+vim.keymap.set('n', '<leader>e', neotree_toggle, { desc = 'Toggle File [E]xplorer' })
+
+-- Move.nvim actions
+vim.keymap.set('n', '<A-j>', ':MoveLine(1)<cr>', { desc = 'Move line up' })
+vim.keymap.set('n', '<A-k>', ':MoveLine(-1)<cr>', { desc = 'Move line down' })
+vim.keymap.set('n', '<A-h>', ':MoveHChar(1)<cr>', { desc = 'Move character right' })
+vim.keymap.set('n', '<A-h>', ':MoveHChar(-1)<cr>', { desc = 'Move character left' })
+vim.keymap.set('v', '<A-j>', ':MoveBlock(1)<cr>', { desc = 'Move block up' })
+vim.keymap.set('v', '<A-k>', ':MoveBlock(-1)<cr>', { desc = 'Move block down' })
+vim.keymap.set('v', '<A-h>', ':MoveHBlock(1)<cr>', { desc = 'Move block right' })
+vim.keymap.set('v', '<A-h>', ':MoveHBlock(1)<cr>', { desc = 'Move block right' })
+
+-- ToggleTerm keymaps
+vim.keymap.set('n', '<leader>tt', ':ToggleTerm direction=horizontal<cr>', { desc = 'Toggle bottom terminal' })
+vim.keymap.set('n', '<leader>tf', ':ToggleTerm direction=float<cr>', { desc = 'Toggle floating terminal' })
+vim.keymap.set('n', '<F7>', ':ToggleTerm<cr>', { desc = 'Toggle terminal' })
+vim.keymap.set('t', '<F7>', [[<cmd>ToggleTerm<cr>]], { desc = 'Toggle terminal' })
+vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], { desc = 'Escape from terminal mode' })
+vim.keymap.set('t', '<C-h>', [[<esc><cmd>wincmd h<cr>]], { desc = 'Move to left split' })
+vim.keymap.set('t', '<C-j>', [[<esc><cmd>wincmd j<cr>]], { desc = 'Move to down split' })
+vim.keymap.set('t', '<C-k>', [[<esc><cmd>wincmd k<cr>]], { desc = 'Move to up split' })
+vim.keymap.set('t', '<C-l>', [[<esc><cmd>wincmd l<cr>]], { desc = 'Move to right split' })
+
+-- Session management
+vim.keymap.set('n', '<leader>ss', ':SearchSession<cr>', { desc = '[S]earch [S]ession' })
+vim.keymap.set('n', '<leader>rs', ':SessionRestore<cr>', { desc = '[R]estore [S]ession' })
+
+-- DAP UI
+local dapui = require('dapui')
+local debug_ui = {
+  opened = false,    -- Current state of dapui
+  neotree_opened = false,   -- If neotree was opened before opening the dapui
+  toggleterm_opened = false,  -- If toggle term was opened before the dapui
+}
+local function toggle_debug_ui(opts)
+  debug_ui.opened = not debug_ui.opened
+  if debug_ui.opened then  -- Show debug UI
+    -- Hide neotree
+    if neotree_open then
+      debug_ui.neotree_opened = true
+      neotree_toggle()
+    else debug_ui.neotree_opened = false end
+    -- Hide toggle term
+    debug_ui.toggleterm_opened = false
+    for _, term in ipairs(require('toggleterm.terminal').get_all(false)) do
+      local tu = require'toggleterm.ui'
+      if tu.term_has_open_win(term) then
+        tu.close(term)
+        debug_ui.toggleterm_opened = true
+      end
+    end
+    -- Show debug ui
+    dapui.open(opts)
+  else
+    -- Hide debug ui
+    dapui.close(opts)
+    -- Toggle terminal if it was visible
+    if debug_ui.toggleterm_opened then
+      vim.cmd("ToggleTerm")
+    end
+    -- Show neotree if it was visible
+    if debug_ui.neotree_opened then
+      neotree_toggle()
+    end
+  end
+end
+vim.keymap.set('n', '<F8>', toggle_debug_ui, { desc = '[D]ap[U]i toggle' })
+
+-- Nvim DAP
+local dap = require('dap')
+vim.keymap.set('n', '<F3>', dap.terminate, { desc = 'Stop debugging' })
+vim.keymap.set('n', '<F4>', dap.restart, { desc = 'Restart debugging' })
+vim.keymap.set('n', '<F5>', dap.continue, { desc = 'Start/Continue debugging' })
+vim.keymap.set('n', '<F9>', dap.toggle_breakpoint, { desc = 'Toggle breakpoint' })
+vim.keymap.set('n', '<F10>', dap.step_over, { desc = 'Debugging step over' })
+vim.keymap.set('n', '<F11>', dap.step_into, { desc = 'Debugging step into' })
+vim.keymap.set('n', '<F12>', dap.step_out, { desc = 'Debugging step out' })
+dap.listeners.after.event_initialized["dapui_config"] = function (opts)
+  if not debug_ui.opened then toggle_debug_ui(opts) end
+end
+local function close_debug_ui(opts) if debug_ui.opened then toggle_debug_ui(opts) end end
+dap.listeners.after.event_terminated["dapui_config"] = close_debug_ui
+dap.listeners.before.event_exited["dapui_config"] = close_debug_ui
+
+-- Useful workflow keybindings
+vim.keymap.set('n', '<C-F5>', ':wa<cr>:ToggleTerm<cr>i<C-u>cargo run<cr>', { remap = true, desc = "Rust cargo run" })
+vim.keymap.set('i', '<C-F5>', '<ESC>:wa<cr>:ToggleTerm<cr>i<C-u>cargo run<cr>', { desc = "Rust cargo run" })
+vim.keymap.set('n', '<F29>', ':wa<cr>:ToggleTerm<cr>i<C-u>cargo run<cr>', { remap = true, desc = "Rust cargo run" })
+vim.keymap.set('i', '<F29>', '<ESC>:wa<cr>:ToggleTerm<cr>i<C-u>cargo run<cr>', { desc = "Rust cargo run" })
