4cc0db710cfb4719d38024b052a5925b9cdf0bdc
TheRetikGM <theretikgm@gmail.com>
2024-04-05 12:15:21 +0200

    refactor: Migrate neogen.nvim

---
+++ lua/plugins/neogen.lua
@@ -0,0 +1,13 @@
+-- Doxygen highlights and generation.
+PLUGINS.neogen = {
+  packer = { 'danymat/neogen' },
+  setup = function()
+    require('neogen').setup({
+      -- snippet_engine = "luasnip",
+      snippet_engine = "vsnip"
+    })
+  end,
+  keymaps = function()
+    vim.keymap.set('n', '<leader>dg', require('neogen').generate, { desc = "Generate [D]oxy[G]en comments", noremap = true, silent = true, })
+  end,
+}
