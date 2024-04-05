8a15bfd4fe073a1a8c8203e152466225e646b015
TheRetikGM <theretikgm@gmail.com>
2024-04-05 11:20:52 +0200

    style: Add template comments

--- lua/plugin_common.lua
+++ lua/plugin_common.lua
@@ -4,8 +4,12 @@ PLUGINS = {}
 PLUGIN_TEMPLATE = {
   -- Table to pass to Packer's `use` function.
   packer = {},
+  -- Setuping function for this plugin. NOTE: You need to require the plugin.
   setup = function() end,
+  -- Name of the plugin to setup after
   after = 'plugin name',
+  -- Name of the plugin to setup before
   before = 'plugin name',
+  -- Function that setups the keymapping.
   keymaps = function() end,
 }
